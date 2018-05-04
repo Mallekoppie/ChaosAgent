ff5f5452a099ad69d2afe1d65f048bc59cffbc53
Hendrik Jacobus Barnard <heinrichbarnard@hotmail.com>
2018-05-04 14:06:37 +0200

    Some successfull tests for the http client

--- go/testrunner.go
+++ go/testrunner.go
@@ -2,7 +2,6 @@ package swagger
 
 import (
 	util "mallekoppie/ChaosAgent/util"
-	"net/http"
 )
 
 // Test statistics variables
@@ -13,59 +12,6 @@ var (
 	transactionsPerSecond int32
 	testCollectionName    string
 )
-
-var (
-	httpClient *http.Client
-)
-
-const (
-	MaxIdleConnections int = 20
-	RequestTimeout     int = 5
-)
-
-// init HTTPClient
-func init() {
-	httpClient = createHTTPClient()
-}
-
-// createHTTPClient for connection re-use
-func createHTTPClient() *http.Client {
-	client := &http.Client{
-		Transport: &http.Transport{
-			MaxIdleConnsPerHost: MaxIdleConnections,
-		},
-		Timeout: time.Duration(RequestTimeout) * time.Second,
-	}
-
-	return client
-}
-
-func MakeCall() {
-	var endPoint string = "http://localhost:80/ConnectionTest"
-
-	req, err := http.NewRequest("GET", endPoint, nil)
-	if err != nil {
-		log.Fatalf("Error Occured. %+v", err)
-	}
-
-	// use httpClient to send request
-	response, err := httpClient.Do(req)
-	if err != nil && response == nil {
-		log.Fatalf("Error sending request to API endpoint. %+v", err)
-	} else {
-		// Close the connection to reuse it
-		defer response.Body.Close()
-
-		// Let's check if the work actually is done
-		// We have seen inconsistencies even when we get 200 OK response
-		_, err := ioutil.ReadAll(response.Body)
-		if err != nil {
-			log.Fatalf("Couldn't parse response body. %+v", err)
-		}
-
-		//log.Println("Response Body:", string(body))
-	}
-}
 
 func CoreGetTestStatus() TestStatus {
 	testStatus := TestStatus{}
--- util/utils_test.go
+++ util/utils_test.go
@@ -1,9 +1,14 @@
 package util
 
 import (
+	"log"
+	"net/http"
 	"testing"
+	//"time"
 )
 
+/*
+// Function is slow. Commenting out while testing http functions
 func TestGetCPU(t *testing.T) {
 
 	result := GetCPUStatus()
@@ -12,4 +17,127 @@ func TestGetCPU(t *testing.T) {
 		t.Log("Result: ", result)
 		t.Fail()
 	}
+}
+*/
+
+type Route struct {
+	Pattern     string
+	HandlerFunc http.HandlerFunc
+}
+
+var (
+	HeaderTestChan chan bool
+	Routes         map[int]Route
+	srv            http.Server
+)
+
+const (
+	BasePath string = "http://localhost:9999"
+)
+
+func init() {
+	HeaderTestChan = make(chan bool, 10)
+	Routes = make(map[int]Route)
+	srv = http.Server{Addr: ":9999"}
+	Routes[0] = Route{Pattern: "/TestHttpCallForBasicGet", HandlerFunc: func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		log.Println("Request received")
+	}}
+	Routes[1] = Route{Pattern: "/TestHttpCallForToSendHeaders", HandlerFunc: func(w http.ResponseWriter, r *http.Request) {
+
+		log.Println("HeaderTestRequest received")
+
+		first := false
+		second := false
+		for header := range r.Header {
+
+			if header == "First" {
+				if r.Header.Get(header) == "value" {
+					first = true
+				}
+			} else if header == "Second" {
+				if r.Header.Get(header) == "secondvalue" {
+					second = true
+				}
+			}
+		}
+
+		if first == true && second == true {
+			HeaderTestChan <- true
+		} else {
+			HeaderTestChan <- false
+		}
+
+		w.WriteHeader(http.StatusOK)
+		log.Println("Handler Done")
+	}}
+
+}
+
+func MakeLittleServerToCall(t *testing.T) {
+
+	myMux := http.NewServeMux()
+
+	for r := range Routes {
+		myMux.HandleFunc(Routes[r].Pattern, Routes[r].HandlerFunc)
+	}
+
+	srv.Handler = myMux
+
+	srv.ListenAndServe()
+}
+
+func TestHttpCallForBasicGet(t *testing.T) {
+	go MakeLittleServerToCall(t)
+
+	headers := make(map[string]string)
+	responseCode, _, _ := MakeHttpCall(BasePath+"/TestHttpCallForBasicGet", http.MethodGet, headers, "")
+
+	if responseCode != http.StatusOK {
+		t.Log("Incorrect response code received")
+		t.Fail()
+	}
+
+	t.Log("Received correct response code")
+}
+
+func TestHttpCallForToSendHeaders(t *testing.T) {
+	go MakeLittleServerToCall(t)
+
+	headers := make(map[string]string)
+	headers["first"] = "value"
+	headers["second"] = "secondvalue"
+
+	responseCode, _, _ := MakeHttpCall(BasePath+"/TestHttpCallForToSendHeaders", http.MethodGet, headers, "")
+
+	if responseCode != http.StatusOK {
+		t.Log("Incorrect response code received")
+		t.Fail()
+	}
+
+	t.Log("Waiting for Channel")
+	correctheadersReceived := <-HeaderTestChan
+
+	if correctheadersReceived != true {
+		t.Log("Incorrect headers received")
+		t.Fail()
+	}
+}
+
+/*
+func TestExternalTest(t *testing.T) {
+	headers := make(map[string]string)
+	headers["first"] = "value"
+	headers["test"] = "secondvalue"
+
+	responseCode, _, _ := MakeHttpCall("http://localhost:80/ConnectionTest", http.MethodGet, headers, "")
+
+	if responseCode != http.StatusOK {
+		t.Log("Incorrect response code received")
+		t.Fail()
+	}
+}
+*/
+func TestKillServer(t *testing.T) {
+	srv.Close()
 }
